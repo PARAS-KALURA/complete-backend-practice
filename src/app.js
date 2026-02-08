b2810d8364dbb55f9b0c822884f1ec55090fb7e6
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-02-09 03:48:19 +0530

    CRUD ✅

--- src/app.js
+++ src/app.js
@@ -7,38 +7,50 @@ app.use(express.json());
 
 const notes = [];
 
-app.get("/", (req, res) => {
-    res.send("Helloo Ji");
-} )
-
 app.get("/notes", (req, res) => {
     res.status(200).json({
         message: 'notes fetched successfully',
         notes: notes
-    }) 
-} )
+    })
+})
 
 
 app.post("/notes", (req, res) => {
     notes.push(req.body);
-    
+
     res.status(201).json({
         message: "note created successfully",
     })
 
+})
+
+
+app.patch("/notes/:index", (req, res) => {
+
+  const index = req.params.index
+
+  const {title, description} = req.body;
+
+  notes[index].description = description
+  notes[index].title = title
+
+  res.status(200).json({
+    message: "Updated successfully"
+  })
+
 } )
 
 app.delete("/notes/:index", (req, res) => {
-         
-           const index = req.params.index;
 
-           notes.splice(index,1);
+    const index = req.params.index;
 
-           res.status(200).json({
-            message: "Successfully deleted"
-           })
+    notes.splice(index, 1);
 
-} )
+    res.status(200).json({
+        message: "Successfully deleted"
+    })
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server ${PORT} is running`);
