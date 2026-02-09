9e9bc84a720dda36f5faebee9941a7080c5dcae8
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-02-10 01:11:45 +0530

    Connected to DB

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-    await mongoose.connect();
+    await mongoose.connect("mongodb+srv://Paras:!PT!.7J.nV!nJPd@complete-backend.i7a4hip.mongodb.net/halley");
+
+    console.log("Connected to DB");
+    
+
 }
