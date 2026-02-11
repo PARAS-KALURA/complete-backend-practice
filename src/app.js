5afb5ffaa400beee2b0bcb09058b80b9f8ea298a
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-02-12 00:50:14 +0530

    Get method done

--- src/app.js
+++ src/app.js
@@ -6,55 +6,41 @@ const noteModel = require("./models/note.model")
 
 app.use(express.json());
 
-const notes = [];
 
-app.get("/notes", (req, res) => {
-    res.status(200).json({
-        message: 'notes fetched successfully',
-        notes: notes
+app.post("/notes",  async (req, res) =>  {
+     
+    const data = req.body; // title, description
+
+     await noteModel.create({
+        title: data.title,
+        description: data.description
     })
-})
-
-
-app.post("/notes", (req, res) => {
-    notes.push(req.body);
 
     res.status(201).json({
-        message: "note created successfully",
+        message: "Note created"
     })
-
-})
-
-
-app.patch("/notes/:index", (req, res) => {
-
-  const index = req.params.index
-
-  const {title, description} = req.body;
-
-  notes[index].description = description
-  notes[index].title = title
-
-  res.status(200).json({
-    message: "Updated successfully"
-  })
 
 } )
 
-app.delete("/notes/:index", (req, res) => {
 
-    const index = req.params.index;
+app.get("/notes", async  (req, res) => {
 
-    notes.splice(index, 1);
+    try {
 
-    res.status(200).json({
-        message: "Successfully deleted"
-    })
+        const notes = await noteModel.find(); // find returns array every fking time
 
-})
+   res.status(200).json({
+    message: "Note fetch successfully",
+    notes
+   })
 
-app.listen(3000, () => {
-    console.log(`Server is running`);
-});
+    } catch(error) {
+        res.status(500).json({
+            message: "Server Error"
+    });
+        
+}    
+} )
+
 
 module.exports = app;
