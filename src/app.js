7ffcba9c61d9467496b4e76442636c40bb6c03d0
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-02-12 01:53:05 +0530

    Delete Added

--- src/app.js
+++ src/app.js
@@ -7,11 +7,11 @@ const noteModel = require("./models/note.model")
 app.use(express.json());
 
 
-app.post("/notes",  async (req, res) =>  {
-     
+app.post("/notes", async (req, res) => {
+
     const data = req.body; // title, description
 
-     await noteModel.create({
+    await noteModel.create({
         title: data.title,
         description: data.description
     })
@@ -20,27 +20,59 @@ app.post("/notes",  async (req, res) =>  {
         message: "Note created"
     })
 
-} )
+})
 
 
-app.get("/notes", async  (req, res) => {
+app.get("/notes", async (req, res) => {
 
     try {
-
+        //.findone() for if u want perticular obj
         const notes = await noteModel.find(); // find returns array every fking time
 
-   res.status(200).json({
-    message: "Note fetch successfully",
-    notes
-   })
+        res.status(200).json({
+            message: "Note fetch successfully",
+            notes
+        })
 
-    } catch(error) {
+    } catch (error) {
         res.status(500).json({
             message: "Server Error"
-    });
+        });
+
+    }
+})
+
+app.delete("/notes/:id", async (req, res) => {
+
+
+    try {
+        const id = req.params.id; // const { id } = req.params;
+
+
+        const deletedNote = await noteModel.findOneAndDelete({
+            _id: id
+        })
+
+
+        if (!deletedNote) {
+      return res.status(404).json({
+        message: "Note not found"
+      });
+    }
         
-}    
-} )
+        res.status(200).json({
+            message: 'Deleted Successfully'
+        })
+
+    }
+
+    catch (error) {
+        res.status(500).json({
+            message: " Server Error"
+        })
+    }
+
+})
 
 
 module.exports = app;
